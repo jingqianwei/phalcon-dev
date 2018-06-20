056d34e25de7aa7d0f14cc7ccb0a6368d0cbe4e1
chinwe.jing <chinwe.jing@etocrm.com>
2018-06-20 14:38:59 +0800

    优化phalcon项目代码

--- app/library/Util/Tools.php
+++ app/library/Util/Tools.php
@@ -321,7 +321,7 @@ class Tools
      */
     public static function writeLog($log)
     {
-        $dir = __DIR__ . "/../Log/";
+        $dir = __DIR__ . "/../logs/";
         self::mkDirs($dir);
         $filename = $dir . date("Y-m-d") . ".log";
         file_put_contents($filename, date("Y-m-d H:i:s") . "\t" . $log . PHP_EOL, FILE_APPEND);
